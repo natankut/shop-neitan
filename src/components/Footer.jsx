73344ae2149a94a8e8203f6f7ab86f884db3cead
Natan Kutnowski <nkutnowski87@gmail.com>
2021-07-29 20:14:25 -0300

    agregada barra de nav sin decoracion

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import cssNavBar from "../css/Footer.css";
+
+function Footer() {
+
+    return (
+        <div className="container-fluid">
+
+        </div>
+    )
+
+
+
+}
+
+
+export default Footer();
